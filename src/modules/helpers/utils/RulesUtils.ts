0fe7f8a018a1d392f35e4735f727005214084236
OlegKofman <oleg.kofman@run.ai>
2023-07-23 14:50:40 +0300

    test

---
+++ src/modules/helpers/utils/RulesUtils.ts
@@ -0,0 +1,15 @@
+import { dataUtils } from '../idexUtils';
+import { logger } from '../logger/logger';
+
+export class RulesUtils {
+    public async trailingVsForwardPeRule(symbol: string) {
+        logger.info('Starting PE Rule');
+        if ((await dataUtils.getForwardPE(symbol)) < (await dataUtils.getTrailingPE(symbol))) {
+            logger.info('PE Rule Passed');
+            return true;
+        } else {
+            logger.info('PE Rule Failed');
+            return false;
+        }
+    }
+}
